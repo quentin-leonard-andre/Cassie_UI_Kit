5af312ac3a463a9436966c805749a3e60eef3fb4
quentin-leonard-andre <quentinleonardandre@gmail.com>
2023-08-16 21:41:31 +0200

    Add first link component integration

---
+++ src/components/Link.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export default{
+    props: [
+      'is_button',
+      'is_primary'
+    ]
+  }
+</script>
+
+<template>
+    <a
+      :class="{'button': is_button, 'primary': is_primary}"
+    >
+      <slot></slot>
+    </a>
+</template>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/variables";
+
+  a{
+    padding: $small_space;
+    cursor: pointer;
+    font-size: $normal_font_size;
+    font-family: $main_font;
+    color: $dark_color;
+
+    &.primary{
+      color: $primary_color;
+    }
+
+    &.button{
+      background-color: $highlight_color;
+      border-radius: $very_small_space;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script lang="ts">
+  import Link from "../components/Link.vue";
   export default{
-    
+    components: {
+      Link
+    }
   }
 </script>
 
 <template>
+  <div class="row">
+    <Link>
+      Lien ordinaire
+    </Link>
 
+    <Link
+      :is_primary="true"
+    >
+      Lien primaire
+    </Link>
+
+    <Link
+      :is_button="true"
+    >
+      Lien bouton
+    </Link>
+  </div>
 </template>
+
+<style lang="scss">
+  @import "../assets/scss/variables";
+
+  .row{
+    display: flex;
+    gap: $small_space;
+  }
+</style>
